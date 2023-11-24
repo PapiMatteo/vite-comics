a082502212c29d4124b8b4a823d17fdd3ea79ddd
PapiMatteo <matteopapi.work@gmail.com>
2023-11-24 16:04:52 +0100

    add: AppContact and footer implemented

---
+++ src/components/AppContact.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+    data() {
+        return {
+           contactList: [
+            'footer-facebook.png',
+            'footer-periscope.png',
+            'footer-pinterest.png',
+            'footer-twitter.png',
+            'footer-youtube.png',
+            ] 
+        }
+        
+    },
+    methods: {
+        getImagePath(img) {
+            return new URL(`../assets/img/${img}`, import.meta.url).href;
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="right-bot-footer">
+        <h3>FOLLOW US</h3>
+    
+        <ul>
+            <li v-for="contact in contactList"><a href=""><img :src="getImagePath(contact)" alt=""></a></li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../style/partials/variables' as *;
+@use '../style/partials/mixins' as *;
+.right-bot-footer {
+    padding: 2rem;
+    @include flex(row, center, center);
+    gap: 1.5rem;
+    h3 {
+    color: $primaryColor;
+    }
+    ul{
+        @include flex(row, center, center);
+        gap: 1.5rem;
+        list-style-type: none;
+        li {
+            a{
+                text-decoration: none;
+            }
+        }
+    }
+}
+
+
+</style>
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,45 @@
 <script>
-export default {
+import AppContact from './AppContact.vue';
 
+export default {
+    data() {
+        return {
+            homeLinks: [
+                "Characters",
+                "Comics",
+                "Movies",
+                "TV",
+                "Games",
+                "Videos",
+                "News",
+            ],
+            shopLinks: [
+                "Shop DC",
+                "Shop DC Collectibles"
+            ],
+            dcLinks: [
+                "Terms Of Use",
+                "Privacy policy (New)",
+                "Ad Choices",
+                "Advertising",
+                "Jobs",
+                "Subscriptions",
+                "Talent Workshops",
+                "CPSC Certificates",
+                "Ratings",
+                "Shop Help",
+                "Contact Us"
+            ],
+            siteLinks: [
+                "DC",
+                "MAD Magazine",
+                "DC Kids",
+                "DC Universe",
+                "DC Power Visa"
+            ]
+        };
+    },
+    components: { AppContact }
 }
 </script>
 
@@ -10,12 +49,32 @@ export default {
             <div class="footer-content">
                 <div class="nav">
                     <div class="left-nav">
-                        <div class="home">TESTO</div>
-                        <div class="shop">TESTO</div>
+                        <div class="home">
+                            <h3>DC COMICS</h3>
+                            <ul>
+                                <li v-for="hlink in homeLinks"><a href="">{{ hlink }}</a></li>
+                            </ul>
+                        </div>
+                        <div class="shop">
+                            <h3>SHOP</h3>
+                            <ul>
+                                <li v-for="sLink in shopLinks"><a href="">{{ sLink }}</a></li>
+                            </ul>
+                        </div>
                     </div>
                     <div class="right-nav">
-                        <div class="about-us">TESTO</div>
-                        <div class="sites">TESTO</div>
+                        <div class="about-us">
+                            <h3>DC</h3>
+                            <ul>
+                                <li v-for="dcLink in dcLinks"><a href="">{{ dcLink }}</a></li>
+                            </ul>
+                        </div>
+                        <div class="sites">
+                            <h3>SITES</h3>
+                            <ul>
+                                <li v-for="site in siteLinks"><a href="">{{ site }}</a></li>
+                            </ul>
+                        </div>
                     </div>
 
                 </div>
@@ -26,10 +85,19 @@ export default {
         </div>
         
     </div>
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="button">
+                <a href="">SIGN-UP NOW!</a>
+            </div>
+            <AppContact />
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
+@use "../style/partials/variables" as *;
 
 .footer-nav {
     background-image: url('../assets/img/footer-bg.jpg');
@@ -40,18 +108,64 @@ export default {
 .bg-logo {
     background-image: url('../assets/img/dc-logo-bg.png');
     background-size: contain;
-    width: 400px;
-    height: 400px
+    width: 300px;
+    height: 300px
 }
 
 .footer-content {
     @include flex(row, space-between, flex-start);
 }
 
-.left-nav {
-    @include flex(column, start, start)
+.nav {
+    @include flex();
+    gap: 1.5rem;
+    padding-top: 2rem
 }
 
+.left-nav {
+    @include flex(column, start, start);
+    
+    .home {
+        padding-bottom: 20px
+    }
+}
 
+.right-nav {
+    @include flex(row, center, start);
+    gap: 1.5rem;
+    padding-bottom: 20px
+}
+
+h3 {
+    color: white;
+    margin-bottom: 1rem;
+}
+
+ul {
+    list-style-type: none;
+    li {
+        a {
+            text-decoration: none;
+            color: rgb(85, 84, 84);
+        }
+    }
+}
+
+.footer-bottom {
+    background-color: #303030;
+    .container {
+        @include flex(row, space-between, center);
+        .button {
+            border: 4px solid $primaryColor;
+            padding: 1rem;
+            a {
+                text-align: center;
+                text-decoration: none;
+                display: inline-block;
+                color: white;
+            }
+        }
+    }
+}
 
 </style>
